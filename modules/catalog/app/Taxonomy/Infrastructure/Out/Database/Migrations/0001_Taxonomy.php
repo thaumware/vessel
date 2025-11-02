88ea0b6541acbe6772cdeaf81a5ab079165efde8
J <jotazents@gmail.com>
2025-11-02 11:28:17 -0300

    feat: Añadir migración para la tabla taxonomy_vocabularies

--- modules/catalog/app/Taxonomy/Infrastructure/Out/Database/Migrations/0001_Taxonomy.php
+++ modules/catalog/app/Taxonomy/Infrastructure/Out/Database/Migrations/0001_Taxonomy.php
@@ -1,2 +1,21 @@
 <?php
 
+use App\Shared\Adapters\Database\MigrationsHelper;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up()
+    {
+        Schema::create('taxonomy_vocabularies', function (Blueprint $table) {
+            MigrationsHelper::loadBaseColumns($table);
+            $table->string('name');
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('taxonomy');
+    }
+};
